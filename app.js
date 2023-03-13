09d221d03369ad39293c5e342e560cae20e175ae
thekingvice <villarrealvicente92@gmail.com>
2023-03-13 14:55:17 -0700

    commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const hamburger = document.querySelector(".navigation__hamburger");
+const close = document.querySelector(".navigation__close");
+const mobileNav = document.querySelector(".navigation__mobile");
 
+hamburger.addEventListener("click", () => {
+  mobileNav.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  mobileNav.style.display = "";
+});
