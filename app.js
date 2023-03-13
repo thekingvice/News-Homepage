33afe50898d7bc87cae4a89100eedd000e2157b6
thekingvice <villarrealvicente92@gmail.com>
2023-03-13 15:06:45 -0700

    commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const html = document.querySelector("html");
 const hamburger = document.querySelector(".navigation__hamburger");
 const close = document.querySelector(".navigation__close");
 const mobileNav = document.querySelector(".navigation__mobile");
 
 hamburger.addEventListener("click", () => {
   mobileNav.style.display = "flex";
+  html.style.background = "#e2e0dc";
 });
 
 close.addEventListener("click", () => {
   mobileNav.style.display = "";
+  html.style.background = "";
 });
